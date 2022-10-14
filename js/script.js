29fd16ba7d1547af8f584f072c54be1694656b55
pedro <pedro.henrike2812@gmail.com>
2022-10-14 17:06:08 -0300

    Iniciado projeto ChatBot com JavaScript

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let recepcaoTexto = document.querySelector("#h4-main");
+let resultado = document.querySelector("#resultado");
+
+
+let nome = window.prompt("Digite seu nome:");
+recepcaoTexto.textContent = `Olá ${nome}, use o meu o ChatBot`

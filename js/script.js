c6f4436c2cf4df3ffae1554234178b1f074dc481
pedro <pedro.henrike2812@gmail.com>
2022-10-14 19:11:11 -0300

    Implementado JavaScript

--- js/script.js
+++ js/script.js
@@ -1,6 +1,41 @@
-let recepcaoTexto = document.querySelector("#h4-main");
+let recepcaoSpan = document.querySelector("#span-main");
 let resultado = document.querySelector("#resultado");
+let campoTexto = document.querySelector("#campo-texto")
+let btn = document.querySelector("#btn")
 
 
 let nome = window.prompt("Digite seu nome:");
-recepcaoTexto.textContent = `Olá ${nome}, use o meu o ChatBot`
+while(nome === null || nome == ""){
+  alert('Digite seu nome, por favor!')
+  nome = window.prompt("Digite seu nome:");
+}
+recepcaoSpan.textContent = `${nome}`
+
+function Responder(campo) {
+
+  let resposta = campo.toLowerCase();
+
+  let perguntas = 
+  {
+    "qual seu nome?": "Meu nome é ChatBot!",
+    "como você está?": `Estou ótimo ${nome} :)`,
+    "quantos anos você tem?": "Mais do que a soma de todas as pessoas na Terra :o",
+    "onde você mora?": "Eu moro no coração de todos :D", 
+    "você bebe água?": "Água? O que seria isso?",
+    "qual meu nome?": `Todos da minha espécie sabem seu ${nome}!`,
+  };
+  
+  console.log(perguntas)
+  if(resposta in perguntas){
+    console.log("existe");
+    console.log(perguntas[resposta])
+  }
+  else{console.log('não existe')}
+  
+}
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  Responder(campoTexto.value);
+})
+

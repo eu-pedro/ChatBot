39f46a902dd3fd276945c381a1d99faf4299b131
pedro <pedro.henrike2812@gmail.com>
2023-01-14 10:30:55 -0300

    melhorando algumas estilizações e adicionando o readme

---
+++ README.md
@@ -0,0 +1,29 @@
+## 👨‍🏫Projeto
+**Tudo sobre o ChatBot** é um site criado com propósito de estabelecer uma conexão entre o usuário e o Bot, onde perguntas são feitas pelo usuário e o Bot responde de uma forma funcional.
+<br />
+<br />
+<img src="assets/home.png">
+<br />
+<br />
+
+## 👨🏻‍💻Tecnologias usadas
+Este projeto foi desenvolvido utilizando as seguintes tecnologias:
+  * HTML5
+  * CSS3
+  * JavaScript
+
+## ▶️Como rodar
+  
+ ### **Passo a passo para rodar a aplicação**  
+1. Faça o clone do repositório :
+
+```sh
+  $ git clone https://github.com/pedrinw/ChatBot.git
+```
+
+**Criado com intuito de testar e melhorar meus conhecimento em HTML5, CSS3 e JavaScript**
+
+
+<h4 align="center">
+    👨🏻‍🚀 Feito por Pedro Henrique
+</h4>
--- js/script.js
+++ js/script.js
@@ -10,30 +10,33 @@ while(nome === null || nome == ""){
   nome = window.prompt("Digite seu nome:");
 }
 recepcaoSpan.textContent = `${nome}`
-
 function Responder(campo) {
-
+  console.log(nome)
   let resposta = campo.toLowerCase();
-
   let perguntas = 
   {
-    "qual seu nome?": "Meu nome é ChatBot!",
-    "como você está?": `Estou ótimo ${nome} :)`,
-    "quantos anos você tem?": "Mais do que a soma de todas as pessoas na Terra :o",
-    "onde você mora?": "Eu moro no coração de todos :D", 
-    "você bebe água?": "Água? O que seria isso?",
-    "qual meu nome?": `Todos da minha espécie sabem seu ${nome}!`,
+    "qual seu nome?": " \u{1F916}: Meu nome é ChatBot, prazer!",
+    "como você está?": `\u{1F916}: Tive um dia ótimo até agora, ${nome} :)`,
+    "quantos anos você tem?": `\u{1F916}: Minha idade é um número irracional, incompreendido pela humanidade ‍ \u{1F62E}`,
+    "onde você mora?": "\u{1F916}: Eu moro no coração de todos :D", 
+    "você bebe água?": "\u{1F916}: Água? O que seria isso?",
+    "qual meu nome?": `\u{1F916}: Todos da minha espécie sabem seu nome ${nome}!`,
+    "você é legal": `\u{1F916}: Muito obrigado ${nome}, fico feliz em receber essa notícia!`,
+    "quem criou você?": "\u{1F916}: O nome dele é Pedro Henrique",
+    "1+1": "\u{1F916}: Em meus cálculos, 1 + 1 é igual a 2, acertei?",
+    "sim": "\u{1F916}: Eu sempre acerto ",
   };
-  
-  console.log(perguntas)
   if(resposta in perguntas){
-    console.log("existe");
-    console.log(perguntas[resposta])
+    let respostaBot = perguntas[resposta];
+    resultado.textContent = respostaBot;
+  } else if(campoTexto.value == ""){
+    resultado.textContent = "\u{1F916}: Me pergunte algo!"
   }
-  else{console.log('não existe')}
-  
+  else{
+    let respostaErro = "\u{1F916}: Desculpe, não compreendi a pergunta. Ainda não estou preparado para responder todas as perguntas, verifique acentuações e sinais de pontuação!"
+    resultado.textContent = respostaErro;
+  }
 }
-
 btn.addEventListener("click", (e) => {
   e.preventDefault();
   Responder(campoTexto.value);
